4a09629e40193e48bb8597c7ac0aa39229fe7a83
Ali Örer <191005040@bakircay.edu.tr>
2025-07-05 23:59:39 +0300

    update js

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -1,1 +1,13 @@
 const timerEl = document.getElementById("timer");
+const startButtonEl = document.getElementById("start");
+const stopButtonEl = document.getElementById("stop");
+const resetButtonEl = document.getElementById("reset");
+
+
+let startTim = 0;
+let elapsedTime = 0;
+let timerInterval ;
+
+function startTimer() {
+    startTime = Date.now() - elapsedTime;
+}
