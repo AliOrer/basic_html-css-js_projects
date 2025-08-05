18e05e7fcf3857714c92a06de40935862548be9c
Ali Örer <191005040@bakircay.edu.tr>
2025-08-05 09:15:47 +0300

    Add task addition and deletion to to-do list app
    Implemented functionality to add tasks via button click or Enter key and delete tasks from the list. Updated script.js to handle DOM events and task management.

--- 9-to-do-list-2/script.js
+++ 9-to-do-list-2/script.js
@@ -1,4 +1,36 @@
 //dom elements
-
 const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+
+//add task  on button clik
+addTaskBtn.addEventListener("click", addTask);
+
+//add task on enter key
+taskInput.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") addTask();
+});
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.style.marginLeft = "10px";
+
+
+    deleteBtn.addEventListener("click", function () {
+        taskList.removeChild(li);
+    });
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+}
